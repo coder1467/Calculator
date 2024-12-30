e8c625644e46773e54e5225cd068deb944683b2a
Ahmed <134275679+coder1467@users.noreply.github.com>
2024-12-30 17:35:50 +0500

    completed script

--- script.js
+++ script.js
@@ -14,9 +14,61 @@ function divide(a, b) {
     return a / b;
 }
 
-function operate(a, b, op) {
-    if (op == '+') add(a, b);
-    else if (op == '-') subtract(a, b);
-    else if (op == '*') multiply(a, b);
-    else if (op == '/') divide(a, b);
+function operate(n1, n2, op) {
+    let result;
+
+    if (op == '+') result = add(n1, n2);
+    else if (op == '-') result = subtract(n1, n2);
+    else if (op == '*') result = multiply(n1, n2);
+    else result = divide(n1, n2);
+
+    resultScreen.innerText = result;
 }
+
+let firstNum = '';
+let secondNum = '';
+let operator;
+let isFirstNum = true
+
+function takeInput(button) {
+    let input = button.value;
+
+    if (input == 'C') {
+        inputScreen.innerText = '';
+        resultScreen.innerText = '';
+        return;
+    } else if (input == '=') {
+        if (firstNum == '' || secondNum == '') return;
+        operate(Number(firstNum), Number(secondNum), operator);
+        firstNum = '';
+        secondNum = '';
+        operator = '';
+        isFirstNum = true;
+        inputScreen.innerText = '';
+        return;
+    } else if (input == '+' || input == '-' || input == '*' || input == '/') {
+        if (isFirstNum) {
+            operator = input;
+            isFirstNum = false;
+        } else {
+            operate(Number(firstNum), Number(secondNum), operator);
+            firstNum = '';
+            secondNum = '';
+            operator = '';
+            isFirstNum = true;
+            inputScreen.innerText = '';
+            return;
+        }
+    }
+    else {
+        if (isFirstNum) firstNum += input;
+        else secondNum += input;
+    }
+    inputScreen.innerText += input;
+}
+
+const inputScreen = document.querySelector('.input');
+const resultScreen = document.querySelector('.result');
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach((button) => button.onclick = () => takeInput(button));
